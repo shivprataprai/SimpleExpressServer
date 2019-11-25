7483f67baae3c0bdf224cbdfdafca53547926d99
shiv <shivprataprai11@gmail.com>
2019-11-25 17:22:39 +0530

    date time

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
-app.get('/hellowWorld',(req,res)=>{
-    res.send('hellowworld')
+app.get('/api/timestamp/:date_string?',(req,res)=>{
+    let c = (req.params.date_string)
+    let dat = new Date(c)
+    let dic ={}
+    dic.unix= dat.getTime()
+    dic.utc= dat.toUTCString()
+    res.send(JSON.stringify(dic))
 })
 app.listen(process.env.PORT,()=>console.log('server started'))
