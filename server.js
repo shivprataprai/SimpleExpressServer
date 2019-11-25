b9a95a26a40656c22227cdbf3a7b2a0567208900
shiv <shivprataprai11@gmail.com>
2019-11-25 16:15:33 +0530

    file added

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ const app = express()
 app.get('/hellowWorld',(req,res)=>{
     res.send('hellowworld')
 })
-app.listen(process.env.port,()=>console.log('server started'))
+app.listen(process.env.PORT,()=>console.log('server started'))
