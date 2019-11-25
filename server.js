67cb616ec644a35e2f0215d0eff8cffb98cb0192
shiv <shivprataprai11@gmail.com>
2019-11-25 17:52:28 +0530

    abc

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ const express = require('express')
 const app = express()
 app.get('/api/timestamp/:date_string?',(req,res)=>{
     let c = (req.params.date_string)
-    let dat = new Date(c)
+    let dat
+    if(isNaN(c)){
+       dat = new Date(c)
+    }
+    else{
+        dat = new Date(parseInt(c)*1000)
+    }
     let dic ={}
     dic.unix= dat.getTime()
     dic.utc= dat.toUTCString()
-    res.send(JSON.stringify(dic))
+    res.json(dic)
 })
-app.listen(process.env.PORT,()=>console.log('server started'))
+app.listen(3333,()=>console.log('server started'))
